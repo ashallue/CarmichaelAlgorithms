9ad5ef46a839a2148bfed2bc6637ab53d2bb97a7
Webster <jewebste@thomas.butler.edu>
2024-08-12 10:45:10 -0400

    added precomputation.cpp

---
+++ precomputation.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <vector>
+#include <array>
+
+int main()
+{
+	const uint64_t primes[167] = {
+     3,   5,   7,  11,  13,  17,  19,  23,  29,
+    31,  37,  41,  43,  47,  53,  59,  61,  67,  71,
+    73,  79,  83,  89,  97, 101, 103, 107, 109, 113,
+   127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+   179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
+   233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
+   283, 293, 307, 311, 313, 317, 331, 337, 347, 349,
+   353, 359, 367, 373, 379, 383, 389, 397, 401, 409,
+   419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
+   467, 479, 487, 491, 499, 503, 509, 521, 523, 541,
+   547, 557, 563, 569, 571, 577, 587, 593, 599, 601,
+   607, 613, 617, 619, 631, 641, 643, 647, 653, 659,
+   661, 673, 677, 683, 691, 701, 709, 719, 727, 733,
+   739, 743, 751, 757, 761, 769, 773, 787, 797, 809,
+   811, 821, 823, 827, 829, 839, 853, 857, 859, 863,
+   877, 881, 883, 887, 907, 911, 919, 929, 937, 941,
+   947, 953, 967, 971, 977, 983, 991, 997
+  };
+
+	//
+	// [i][0] stores P
+	// [i][1] stores L = lambda(P)
+	// [i][2] stores b.  If n = PR is a CN, primes dividing R > b.
+	// [i][3] stores k = omega(P), the count of distinct prime divisors of P
+
+	std::vector< std::array<uint64_t, 4> > new_jobs, old_jobs, output_jobs;
+
+	old_jobs.push_back({1, 1, 1, 0});
+
+	double bound = 23*log(10);
+
+	for( uint64_t i = 0; i < 40; i ++ )
+	{
+		uint64_t prime_preprod[4] = { primes[i], primes[i] - 1, primes[i], 1};
+		while( !old_jobs.empty() )
+		{
+				uint64_t current_preproduct[4] = { old_jobs.back()[0], old_jobs.back()[1],old_jobs.back()[2],old_jobs.back()[3] };
+				old_jobs.pop_back();
+
+				// This is the log version of P*L*p^4 > B
+				if( log( current_preproduct[0]) + log( current_preproduct[1]) + 4*log( primes[i] ) > bound )
+				{
+					output_jobs.push_back( {current_preproduct[0],current_preproduct[1],current_preproduct[2],current_preproduct[3]}  );
+				}
+				else // so current_preproduct is small enough to create more jobs
+				{
+					new_jobs.push_back( {current_preproduct[0],current_preproduct[1],primes[i], current_preproduct[3] } );
+					if( std::gcd( current_preproduct[0], primes[i] - 1 ) == 1 )
+					{
+						new_jobs.push_back({ current_preproduct[0]*primes[i], current_preproduct[1]*((primes[i] - 1)/ std::gcd(current_preproduct[1], primes[i] -1 ) ), primes[i], current_preproduct[3]++ });
+					}
+
+				}
+			}
+			std::cout << " The prime " << primes[i] << " has " << new_jobs.size() << " working jobs and " << output_jobs.size() << " are output jobs" << std::endl;
+			old_jobs = new_jobs;
+			new_jobs.clear();
+	}
+
+	// output the two jobs lists here:
+
+
+}
