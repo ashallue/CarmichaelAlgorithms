29c55a6c2235783430a633d7b5eab6a737633a8c
Webster <jewebste@thomas.butler.edu>
2024-08-13 11:18:08 -0400

    fixed spacing

--- precomputation.cpp
+++ precomputation.cpp
@@ -8,7 +8,7 @@
 
 int main()
 {
-	const uint64_t primes[167] = {
+  const uint64_t primes[167] = {
      3,   5,   7,  11,  13,  17,  19,  23,  29,
     31,  37,  41,  43,  47,  53,  59,  61,  67,  71,
     73,  79,  83,  89,  97, 101, 103, 107, 109, 113,
@@ -29,87 +29,86 @@ int main()
   };
 
 
-	std::vector< std::array<uint64_t, 3> > new_jobs, old_jobs, output_jobs;
-	// The order of the 3-tuple {P, L, b }
-	// P, the preproduct
-	// L = CarmichaelLambda(P)
-	// if n = PR is a CN from a 4-tuple, then the primes dividing R have to exceed b
+  std::vector< std::array<uint64_t, 3> > new_jobs, old_jobs, output_jobs;
+  // The order of the 3-tuple {P, L, b }
+  // P, the preproduct
+  // L = CarmichaelLambda(P)
+  // if n = PR is a CN from a 4-tuple, then the primes dividing R have to exceed b
 
-	// The trivial preproduct
-	old_jobs.push_back({1, 1, 1});
+  // The trivial preproduct
+  old_jobs.push_back({1, 1, 1});
 
-	// intended bound for the computation is 10^23
-	// bounds testing is done with logarithms
-	double bound = 23*log(10);
+  // intended bound for the computation is 10^23
+  // bounds testing is done with logarithms
+  double bound = 23*log(10);
 
-	uint64_t prime_count;
-	std::cout << "How many primes to use? " ;
-	std::cin >> prime_count ;
+  uint64_t prime_count;
+  std::cout << "How many primes to use? " ;
+  std::cin >> prime_count ;
 
-	std::cout << "The elimination rule is of the form P*L*f(p) where, " << std::endl;
-	std::cout << " f(p) = C*p^n and p is the current prime. " << std::endl;
-	std::cout << "The paper currently recommends n = 4 and C = 1" << std::endl;
-	std::cout << "What power n of p do you want? " ;
-	uint64_t p_exponent;
-	std::cin >> p_exponent;
-	std::cout << "What constant C do you want? " ;
-	uint64_t C_constant;
-	std::cin >> C_constant;
-	std::cout << std::endl;
+  std::cout << "The elimination rule is of the form P*L*f(p) > B where, " << std::endl;
+  std::cout << " f(p) = C*p^n and p is the current prime. " << std::endl;
+  std::cout << "The paper currently recommends n = 4 and C = 1" << std::endl;
+  std::cout << "What power n of p do you want? " ;
+  uint64_t p_exponent;
+  std::cin >> p_exponent;
+  std::cout << "What constant C do you want? " ;
+  uint64_t C_constant;
+  std::cin >> C_constant;
+  std::cout << std::endl;
 
-	bound = bound - log(C_constant);
+  bound = bound - log(C_constant);
 
-	for( uint64_t i = 0; i < prime_count; i ++ )
-	{
-		uint64_t prime_preprod[3] = { primes[i], primes[i] - 1, primes[i]};
-		while( !old_jobs.empty() )
-		{
-				uint64_t current_preproduct[3] = { old_jobs.back()[0], old_jobs.back()[1], old_jobs.back()[2] };
-				old_jobs.pop_back();
+  for( uint64_t i = 0; i < prime_count; i ++ )
+  {
+        while( !old_jobs.empty() )
+    {
+        uint64_t current_preproduct[3] = { old_jobs.back()[0], old_jobs.back()[1], old_jobs.back()[2] };
+        old_jobs.pop_back();
 
-				if( log( current_preproduct[0]) + log( current_preproduct[1]) + p_exponent*log( primes[i] ) > bound )
-				{
-					output_jobs.push_back( { current_preproduct[0], current_preproduct[1], current_preproduct[2] }  );
-				}
-				else // so current_preproduct is small enough to create more jobs
-				{
-					new_jobs.push_back( { current_preproduct[0], current_preproduct[1], primes[i] } );
-					if( std::gcd( current_preproduct[0], primes[i] - 1 ) == 1 )
-					{
-						new_jobs.push_back( { current_preproduct[0]*primes[i], current_preproduct[1]*((primes[i] - 1)/ std::gcd(current_preproduct[1], primes[i] -1 ) ), primes[i] });
-					}
+        if( log( current_preproduct[0]) + log( current_preproduct[1]) + p_exponent*log( primes[i] ) > bound )
+        {
+          output_jobs.push_back( { current_preproduct[0], current_preproduct[1], current_preproduct[2] }  );
+        }
+        else // so current_preproduct is small enough to create more jobs
+        {
+          new_jobs.push_back( { current_preproduct[0], current_preproduct[1], primes[i] } );
+          if( std::gcd( current_preproduct[0], primes[i] - 1 ) == 1 )
+          {
+            new_jobs.push_back( { current_preproduct[0]*primes[i], current_preproduct[1]*((primes[i] - 1)/ std::gcd(current_preproduct[1], primes[i] -1 ) ), primes[i] });
+          }
 
-				}
-			}
-			std::cout << " The prime " << primes[i] << " has " << new_jobs.size() << " working jobs and " << output_jobs.size() << " are output jobs" << std::endl;
-			old_jobs = new_jobs;
-			new_jobs.clear();
-	}
+        }
+      }
+      std::cout << " The prime " << primes[i] << " has " << new_jobs.size() << " working jobs and " << output_jobs.size() << " are output jobs" << std::endl;
+      old_jobs = new_jobs;
+      new_jobs.clear();
+  }
 
 
-	// output the two jobs lists here:
-	/*
-	std::ofstream output_file("output_jobs.txt");
-	for( int i = 0; i < output_jobs.size(); i++ )
-	{
-		for( int j = 0; j < 3; j++)
-		{
-		 	output_file << output_jobs[i][j] << " ";
-		}
-			output_file << std::endl;
-	}
-	output_file.close();
+  // output the two jobs lists here:
+  /*
+  std::ofstream output_file("output_jobs.txt");
+  for( int i = 0; i < output_jobs.size(); i++ )
+  {
+    for( int j = 0; j < 3; j++)
+    {
+       output_file << output_jobs[i][j] << " ";
+    }
+      output_file << std::endl;
+  }
+  output_file.close();
 
-	std::ofstream working_file("working_jobs.txt");
-	for( int i = 0; i < old_jobs.size(); i++ )
-	{
-		for( int j = 0; j < 3; j++)
-		{
-		 	working_file << old_jobs[i][j] << " ";
-		}
-			working_file << std::endl;
-	}
-	working_file.close();
+  std::ofstream working_file("working_jobs.txt");
+  for( int i = 0; i < old_jobs.size(); i++ )
+  {
+    for( int j = 0; j < 3; j++)
+    {
+       working_file << old_jobs[i][j] << " ";
+    }
+      working_file << std::endl;
+  }
+  working_file.close();
  */
 
 }
