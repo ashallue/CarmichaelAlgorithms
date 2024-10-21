15258b1986e580c9ad877ecbfc7908e1f06071bb
Webster <jewebste@thomas.butler.edu>
2024-10-21 07:54:03 -0400

    comments added to is_admissible

--- Preproduct.cpp
+++ Preproduct.cpp
@@ -3,11 +3,14 @@
 
 bool Preproduct::is_admissable( int64_t prime_to_append )
 {
+  // if this works how we want it to, this while loop will not be entered often
   while( prime_to_append > next_inadmissable[0] )
   {
+    // add 2*p or 4*p and avoid divisibility be 3.  Flip the state of mod 3 status.
     next_inadmissable[0] += (appended_primes[0] << mod_three_status[0]);
     mod_three_status[0] = (mod_three_status[0] == 1 ) ? 2 : 1;
     int i= 1;
+    // put next_inadmissable in sorted order having increased the first element
     while( i < len_appended_primes && next_inadmissable[ i-1 ] < next_inadmissable[ i ] )
     {
       std::swap( next_inadmissable[ i-1 ], next_inadmissable[ i ] );
@@ -16,5 +19,7 @@ bool Preproduct::is_admissable( int64_t prime_to_append )
       i++;
     }
   }
+  // now prime to append is less than or equal to next_inadmissable
+  // return true if it is less than; return false if it is equal
   return ( prime_to_append < next_inadmissable[0] ) ;
 }
