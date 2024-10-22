f64dd6113de75afe6e2cad8a1a141a507f580454
Webster <jewebste@thomas.butler.edu>
2024-10-22 09:38:24 -0400

    initilization improvements

--- Preproduct.cpp
+++ Preproduct.cpp
@@ -1,60 +1,64 @@
 #include "Preproduct.h"
 #include <algorithm>
+#include <iostream>
 #include <cstdint>
 #include <stdio.h>
 #include <gmp.h>
 
-Preproduct::Preproduct( uint64_t init_preproduct, uint64 init_LofP, uint64 init_append_bound )
+// could consider a constructor that passes in the factors of P and lambda(P)
+// does not check that init_LofP is actually lambda(P)
+// right now this uses trial division
+// intended use case is relatively small initializing preproducts
+Preproduct::Preproduct( uint64_t init_preproduct, uint64_t init_LofP, uint64_t init_append_bound )
 {
-  uint64_t = temp;
-  temp = init_preproduct;
+  	uint64_t temp;
+  	temp = init_preproduct;
 
-  mpz_init( P ) ;
-  mpz_import( P, 1, 1, sizeof(uint64_t), 0, 0, &temp );
-  L = init_LofP;
-  append_bound = init_append_bound;
+  	mpz_init( P ) ;
+  	mpz_import( P, 1, 1, sizeof(uint64_t), 0, 0, &temp );
+  	L = init_LofP;
+	append_bound = init_append_bound;
 
 	P_len = 0;
 
-  if( init_prepoduct != 1 )
-  {
-	  temp = 1;
-	  while( temp*temp < init_preproduct )
-	  {
-		  temp+= 2;
-		  if( ( init_preproduct % temp ) == 0 )
-		  {
-			  init_preproduct = init_preproduct / temp;
-			  P_primes[ P_len ] = temp;
-			  P_len++;
-		  }
-	  }
-	  // possible remaining factor
-	  if( init_preproduct > 1 )
-	  {
-		  P_primes[ P_len ] = init_preproduct;
-		  P_len++;
-	  }
-	  
-  }
-
-  if( init_LofP == 1 )
-  { 
-     L_len = 0;
-   }
-  else
-  {
+  	// factory by trial division
+  	if( init_preproduct != 1 )
+  	{
+		temp = 1;
+	  	while( temp*temp < init_preproduct )
+	  	{
+		  	temp+= 2;
+		  	if( ( init_preproduct % temp ) == 0 )
+		  	{
+			  	init_preproduct = init_preproduct / temp;
+			  	P_primes[ P_len ] = temp;
+			  	P_len++;
+		  	}
+	  	}
+	  	// possible remaining factor
+	  	if( init_preproduct > 1 )
+	  	{
+		  	P_primes[ P_len ] = init_preproduct;
+		  	P_len++;
+	  	}
+	}
+	
+	if( init_LofP == 1 )
+  	{ 
+		L_len = 0;
+   	}
+	else
+  	{
      //factor L here for L_exponents, L_distinct_primes, and L_len
-   }
-
-  appended_primes = 0;     
-
+   	}
+  	len_appended_primes = 0;     
 }
 
 
 // admissibility check with no gcd
 // if the while loop is not taken, this will execute with less than 10 instructions
-bool Preproduct::is_admissible( int64_t prime_to_append )
+// assumes len_appended_primes > 0
+bool Preproduct::is_admissible( uint64_t prime_to_append )
 {
   // if this works how we want it to, this while loop will not be entered often
   while( prime_to_append > next_inadmissible[0] )
@@ -75,4 +79,15 @@ bool Preproduct::is_admissible( int64_t prime_to_append )
   // now prime to append is less than or equal to next_inadmissible
   // return true if it is less than; return false if it is equal
   return ( prime_to_append < next_inadmissible[0] ) ;
+}
+
+
+int main(void) {
+
+   Preproduct P0( 143, 60, 13 ); 
+
+   std::cout << "Initializing Preproduct : " << 143 << std::endl;
+   
+
+   return 0;
 }
--- Preproduct.h
+++ Preproduct.h
@@ -1,27 +1,33 @@
 #ifndef PREPRODUCT_H
 #define PREPRODUCT_H
 
+#include <algorithm>
+#include <cstdint>
+#include <stdio.h>
+#include <gmp.h>
 
 class Preproduct{
-  public:
+    
+	
+public:
 
-    const mpz_t P;
-    const uint64_t P_primes[20];
-    const uint16_t P_len;
-    const uint64_t append_bound;  // primes appended to P need to exceed this bound
+	mpz_t P;
+    uint64_t P_primes[20];
+    uint16_t P_len;
+    uint64_t append_bound;  // primes appended to P need to exceed this bound
 
     // Information about L = CarmichaelLambda(P)
-    const uint64_t L;
-    const uint64_t L_distinct_primes[20];
-    const uint16_t L_exponents[20];   
-    const uint16_t L_len;
+    uint64_t L;
+    uint64_t L_distinct_primes[20];
+    uint16_t L_exponents[20];   
+    uint16_t L_len;
 
     // 5 is chosen because that's the limit of the recursion depth.
-    const uint16_t len_appended_primes;
+    uint16_t len_appended_primes;
 
     // these arrays are used to avoid gcd computations for admissibility checks
     // they are updated assuming primes are tested for admissibility in increasing order
-    // These three arrays are the only non const type - requires updating
+    // These three arrays are the only data members that can change after initialization
     uint64_t next_inadmissible[5]; 
     uint16_t mod_three_status[5];  
     uint64_t appended_primes[5];   
@@ -30,8 +36,9 @@ class Preproduct{
     // constructor and destructor here
     Preproduct( uint64_t init_preproduct, uint64_t init_LofP, uint64_t init_append_bound );
 
-    //
+	// member functions
     bool is_admissible( uint64_t prime_to_append );
+
 
 };
 
