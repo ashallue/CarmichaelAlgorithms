bbe63159b5177e5b2a417ed1f11c3e529f21ffe8
Webster <jewebste@thomas.butler.edu>
2025-02-25 09:39:29 -0500

    aggregated 'improvement' comments at the top and cleaned up the remaining comments

--- Preproduct.h
+++ Preproduct.h
@@ -13,6 +13,32 @@ class Preproduct{
     
 
 public:
+    
+    // lower level improvements that are currently not implemented:
+    // 1 - we can do this with uint128_t and avoid GMP entirely
+    //      1a - the primary need is something faster than GMP to compute 2^(n-1) mod n
+    //           this is the primary elimination check in CN_search
+    //           for P = 515410417841, we did 16,737,417 modular exponentiations and only found 126 base 2 psps
+    //      1b - while writing a new mdoular exponentiation routine, we could integrate CN_factorization into it
+    //           I think (needs to be checked) that once we transform to the Montgomery representation, the gcds
+    //           can be done in that representation.  So, we do not need to enter-exit as we do right now
+    //      1c - most of the other number theoretic functions can be found in std or are more easily written
+    // 2 - partitition the P_primes vector into by prime size
+    //      2a - for small primes use fast remainders for admissibility, e.g.:
+    //           https://arxiv.org/abs/1902.01961
+    //      2b - for other primes, keep store the next least inadmissible candidate
+    //           then admissbility is checking an inequality (updating data-structure if not)
+    //           by chooseing the not-small primes large enough the data structure should not be updated often
+    // 3 - store explicit prime factorization of L and q-1, both primes and their exponents
+    //           updating L can then be done with a merge of
+    //           the merge and multiplications have to be cheaper than the Euclican algorithm approach with LCM( L ,q-1)
+    // 4 - At all stages we compute P^{-1} mod L.  Is it possible to incrementally update this quantity?
+    //           That is, given P, P^{-1} mod L, q a prime admissible to P (and whatever prime factorizations we might want)
+    //           can we compute (Pq)^{-1} mod LCM( L , q-1 ) faster than the obvious method?
+    // 5 - A better implementation (or two) of completing_with_exactly_one_prime
+    //           5a - For case 3 of complete_tabulation, the expection is that this results in the bounded factorization
+    //           5b - For case 1 of complete_tabulation, the expecation is tha this results in the unbounded factorization
+    //                  so, maybe this can be done faster by using Lenstra's version rather than our modified trial divison
 
     // constructor always sets to 10^24
     mpz_t BOUND;
@@ -20,21 +46,10 @@ public:
     // stores P and the primes dividing p in sorted order
     mpz_t P;
     std::vector< uint64_t > P_primes;
-    
-    // previous versions had extra data associated to P_primes
-    // would store candidate for inadmissibility so that admissibiliy checks were inequality
-    // if the inequality failed, an "easy" update would restore the data strucutre updating the least candidate
-    // this is still viable - a hybrid of mod checks for small primes and the data structure for larger primes
-   
+      
     // stores L
     mpz_t L;
     
-    // previous versions had extra data assocaited with L
-    // if the complete prime factorization of L and p-1 are known
-    // then LCM( L, p - 1) may be computed via a merge
-    // and the euclidaean algorithm need not be invoked
-    // we might revisit this but in the present version we do not do this and removed this support
-
     // primes appended to P need to exceed this bound
     uint64_t append_bound;
 
@@ -43,27 +58,21 @@ public:
     ~Preproduct();
     
     // initializing call
-    // has to factor init_preproduct and init_LofP
+    // has to factor init_preproduct
     void initializing( uint64_t init_preproduct, uint64_t init_LofP, uint64_t init_append_bound );
     
     // appending call
-    // assume we have an admissible prime to append.
+    // we assume the prime is admissible to PP
     void appending( Preproduct& PP, uint64_t prime );
     
-    // member functions
-       
     // done with no gcd check - checks if input is not 1 mod p for all p | P
-    // consdier Lemire's fast remainders (?)
-    // see also comment above
     bool is_admissible_modchecks( uint64_t prime_to_append );
-    
     
     // uses a rule to decide whether to call CN_search or
     // initialize an incremental sieve and do prime-by-prime appending
     // for primes q in (append_bound, (B/P)^(1/3) ) we recursively call this on preproduct Pq
     // for primes q in ((B/P)^(1/3) , (B/P)^(1/2) ) we do the factorization on Pq-1
     void complete_tabulation( std::string cars_file );
-    
     
     // This does *not* create a Preproduct structure
     bool appending_is_CN( std::vector< uint64_t >&  primes_to_append );
@@ -76,15 +85,8 @@ public:
     // calls CN_factorization when P*R ius a base 2 and base 3 Fermat psp
     void CN_search( std::string cars_file  );
     
-    
     // For some preproducts, we will still only need to find exactly one prime
     // Implements the modified trial division of Serction 5.3 ANTS 2024 paper
-    // We could consider two version:
-    // Version 1 - a custom version of CN_search that only outputs primes (consider this the bounded factorization)
-    // Version 2 - the Lenstra Divisors in residue class algorithm
-    // Contrary-wise:
-    // Our implementation accounts for the bounded case (although does not do prime sieving when bounded)
-    // and uses residue class information in a straighforward way (easier to check the correctness)
     void completing_with_exactly_one_prime( );
     
     // check that L exactly divides P - 1
@@ -102,7 +104,6 @@ public:
     // but that for CN the probability is 7/8
     // I do not know where the 7/8 comes from
     bool CN_factorization( mpz_t& n, mpz_t& R, std::vector<uint64_t>& R_prime_factors, std::string cars_file );
-
 
 };
 
