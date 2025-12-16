7d26ae760d4f1b7aec2f76496791c8872d5ee0ad
jewebste <jewebste@butler.edu>
2025-12-16 11:53:55 -0500

    Create rollsieve_test.cpp

---
+++ IncrementalSieve/rollsieve_test.cpp
@@ -0,0 +1,40 @@
+#include "rollsieve.h"
+#include <iostream>
+#include <algorithm>
+#include <gmp.h>
+#include <numeric>
+
+int main(void) {
+    
+    std::vector< uint64_t > pm1_factors;
+
+    Rollsieve r(1);
+
+    uint64_t p = r.getn();
+    
+    while( p < 400 )
+    {
+        r.next();
+        p = r.getn();
+    }
+    
+    
+    while( p < 600 )
+    {
+        r.getlist( pm1_factors );
+        std::cout << "prime factors of " << p << " are " ;
+        std::sort( pm1_factors.begin(), pm1_factors.end() );
+        for( auto f : pm1_factors )
+        {
+            std::cout << f << " " ;
+        }
+        std::cout << std::endl;
+
+        r.next();
+        p = r.getn();
+    }
+    
+
+
+    return 0;
+}
